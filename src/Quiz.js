09b17c4d5e43ec8103b9d47921ff1070e2e5a869
Nick McCurdy <nick@nickmccurdy.com>
2018-04-09 14:43:11 -0400

    Make Quiz a class component

--- src/Quiz.js
+++ src/Quiz.js
@@ -9,8 +9,45 @@ import {
   Typography,
   withStyles
 } from 'material-ui';
-import React, { Fragment } from 'react';
+import React, { Component, Fragment } from 'react';
 import quizzes from './quizzes';
+
+class Quiz extends Component {
+  render() {
+    const {
+      classes: { stepper, title },
+      match: { params: { name } }
+    } = this.props;
+
+    return (
+      <Fragment>
+        <Typography variant="title" className={title}>
+          {name}
+        </Typography>
+
+        <Stepper orientation="vertical" className={stepper}>
+          {quizzes.find(quiz => quiz.name === name).questions.map(question => (
+            <Step key={question.name}>
+              <StepLabel>{question.name}</StepLabel>
+
+              <StepContent>
+                <RadioGroup>
+                  {Object.keys(question.options).map(option => (
+                    <FormControlLabel
+                      key={option}
+                      control={<Radio checked={false} />}
+                      label={option}
+                    />
+                  ))}
+                </RadioGroup>
+              </StepContent>
+            </Step>
+          ))}
+        </Stepper>
+      </Fragment>
+    );
+  }
+}
 
 export default withStyles(theme => ({
   stepper: {
@@ -21,30 +58,4 @@ export default withStyles(theme => ({
     paddingBottom: 16,
     marginTop: theme.spacing.unit * 3
   })
-}))(({ classes: { stepper, title }, match: { params: { name } } }) => (
-  <Fragment>
-    <Typography variant="title" className={title}>
-      {name}
-    </Typography>
-
-    <Stepper orientation="vertical" className={stepper}>
-      {quizzes.find(quiz => quiz.name === name).questions.map(question => (
-        <Step key={question.name}>
-          <StepLabel>{question.name}</StepLabel>
-
-          <StepContent>
-            <RadioGroup>
-              {Object.keys(question.options).map(option => (
-                <FormControlLabel
-                  key={option}
-                  control={<Radio checked={false} />}
-                  label={option}
-                />
-              ))}
-            </RadioGroup>
-          </StepContent>
-        </Step>
-      ))}
-    </Stepper>
-  </Fragment>
-));
+}))(Quiz);
