04264c8eaf49ad47369a65bae05ae12c8d99f8ba
Nick McCurdy <nick@nickmccurdy.com>
2018-05-05 16:35:24 -0400

    Format files

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import 'typeface-roboto'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
-import 'typeface-roboto'
 
 ReactDOM.render(<App />, document.getElementById('root'))
 registerServiceWorker()
