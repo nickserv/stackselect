4aac7bebe0d93e26ffd4e7b71193d52bdadcb8aa
Nick McCurdy <nick@nickmccurdy.com>
2018-05-16 06:26:51 -0400

    Update react-scripts-ts

--- src/index.tsx
+++ src/index.tsx
@@ -2,5 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'typeface-roboto';
 import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
