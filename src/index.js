9fc9caeae7d0aec92d8027ac1b29cc52d9cbfd81
Nick McCurdy <nick@nickmccurdy.com>
2018-05-23 08:23:18 -0400

    Use HashRouter

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import 'typeface-roboto'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById('root')
 )
 registerServiceWorker()
