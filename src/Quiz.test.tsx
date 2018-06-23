f440d94248470e5785cffef06b4858aa8583b073
Nick McCurdy <nick@nickmccurdy.com>
2018-06-23 09:31:23 -0400

    Test option radios and invalid quizzes

--- src/Quiz.test.tsx
+++ src/Quiz.test.tsx
@@ -4,6 +4,9 @@ import { fireEvent, render } from 'react-testing-library'
 import Quiz from './Quiz'
 import quizzes from './quizzes.json'
 
+beforeEach(() => (console.error = jest.fn()))
+afterEach(jest.restoreAllMocks)
+
 test('Quiz', () => {
   const { name, questions } = quizzes[0]
   const { container, getByText } = render(
@@ -21,8 +24,29 @@ test('Quiz', () => {
   for (const { name } of questions[0].answers)
     expect(container).toHaveTextContent(name)
 
+  // select an option
+  const option = container.querySelector('input[value=MVC]') as HTMLInputElement
+  expect(option).toHaveProperty('checked', false)
+  fireEvent.click(option)
+  expect(option).toHaveProperty('checked', true)
+
   // change questions
   fireEvent.click(getByText(questions[1].name))
   for (const { name } of questions[1].answers)
     expect(container).toHaveTextContent(name)
+})
+
+test('invalid Quiz', () => {
+  expect(() =>
+    render(
+      <Quiz
+        match={{
+          isExact: true,
+          params: { name: 'Invalid' },
+          path: '/:name',
+          url: '/Invalid'
+        }}
+      />
+    )
+  ).toThrow('Quiz not found: Invalid')
 })
