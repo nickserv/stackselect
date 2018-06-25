ba546e0df8a7afdd3678e2f41bbf6366d202eddb
Nick McCurdy <nick@nickmccurdy.com>
2018-06-25 19:12:33 -0400

    Use jest-each

--- src/getOptions.test.ts
+++ src/getOptions.test.ts
@@ -1,65 +1,59 @@
+import each from 'jest-each'
 import getOptions, { flatten, unique } from './getOptions'
 import quizzes from './quizzes.json'
 
-test('flatten', () => {
-  expect(flatten([])).toEqual([])
-  expect(flatten([[1, 2]])).toEqual([1, 2])
-  expect(flatten([[1, 2], [3, 4]])).toEqual([1, 2, 3, 4])
-})
+each([[[], []], [[[1, 2]], [1, 2]], [[[1, 2], [3, 4]], [1, 2, 3, 4]]]).test(
+  'flatten',
+  (array, expected) => {
+    //@ts-ignore https://github.com/DefinitelyTyped/DefinitelyTyped/pull/26836
+    expect(flatten(array)).toEqual(expected)
+  }
+)
 
-test('unique', () => {
-  expect(unique([])).toEqual([])
-  expect(unique([1, 2, 3])).toEqual([1, 2, 3])
-  expect(unique([1, 2, 2, 3])).toEqual([1, 2, 3])
-})
+each([[[], []], [[1, 2, 3], [1, 2, 3]], [[1, 2, 2, 3], [1, 2, 3]]]).test(
+  'unique',
+  (array, expected) => {
+    //@ts-ignore https://github.com/DefinitelyTyped/DefinitelyTyped/pull/26836
+    expect(unique(array)).toEqual(expected)
+  }
+)
 
-test('getOptions', () => {
-  const questions = quizzes[0].questions
-  expect(getOptions(questions)).toEqual([
-    'Angular',
-    'None',
-    'Polymer',
-    'React',
-    'Vue',
-    'Web Components'
-  ])
-  expect(getOptions(questions, {})).toEqual([
-    'Angular',
-    'None',
-    'Polymer',
-    'React',
-    'Vue',
-    'Web Components'
-  ])
-  expect(
-    getOptions(questions, {
-      'What architecture do you want to follow?': 'MVC'
-    })
-  ).toEqual(['Angular'])
-  expect(
-    getOptions(questions, {
+const questions = quizzes[0].questions
+
+each([
+  [undefined, ['Angular', 'None', 'Polymer', 'React', 'Vue', 'Web Components']],
+  [{}, ['Angular', 'None', 'Polymer', 'React', 'Vue', 'Web Components']],
+  [{ 'What architecture do you want to follow?': 'MVC' }, ['Angular']],
+  [
+    {
       'What platforms do you need to support other than frontend web?':
         'Server side rendering'
-    })
-  ).toEqual(['React'])
-  expect(
-    getOptions(questions, {
+    },
+    ['React']
+  ],
+  [
+    {
       'What architecture do you want to follow?': 'Component driven',
       'What platforms do you need to support other than frontend web?':
         'Server side rendering'
-    })
-  ).toEqual(['React'])
-  expect(
-    getOptions(questions, {
+    },
+    ['React']
+  ],
+  [
+    {
       'What architecture do you want to follow?': 'Component driven',
       'What platforms do you need to support other than frontend web?': 'Native'
-    })
-  ).toEqual(['React', 'Vue'])
-  expect(
-    getOptions(questions, {
+    },
+    ['React', 'Vue']
+  ],
+  [
+    {
       'What architecture do you want to follow?': 'None',
       'What platforms do you need to support other than frontend web?':
         'Server side rendering'
-    })
-  ).toEqual([])
+    },
+    []
+  ]
+]).test('getOptions', (answers, expected) => {
+  expect(getOptions(questions, answers)).toEqual(expected)
 })
