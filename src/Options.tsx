5cd07ebe42760b9a5ce0a711dc49d7524e689f65
Nick McCurdy <nick@nickmccurdy.com>
2018-07-04 20:50:48 -0400

    Remove extra args from Options
    This reverts commit 1ee078dcdc5e7ccfde18e9cc7331987ce1fd45ab.

--- src/Options.tsx
+++ src/Options.tsx
@@ -4,16 +4,14 @@ import FlipMove from 'react-flip-move'
 
 const styles = ({ spacing: { unit } }: Theme) => ({ chip: { margin: unit } })
 
-interface Props extends WithStyles<typeof styles>, FlipMove.FlipMoveProps {
+interface Props extends WithStyles<typeof styles> {
   options: string[]
 }
 
-export default withStyles(styles)(
-  ({ classes: { chip }, options, ...other }: Props) => (
-    <FlipMove typeName="span" {...other}>
-      {options.map(option => (
-        <Chip key={option} className={chip} label={option} component="span" />
-      ))}
-    </FlipMove>
-  )
-)
+export default withStyles(styles)(({ classes: { chip }, options }: Props) => (
+  <FlipMove typeName="span">
+    {options.map(option => (
+      <Chip key={option} className={chip} label={option} component="span" />
+    ))}
+  </FlipMove>
+))
